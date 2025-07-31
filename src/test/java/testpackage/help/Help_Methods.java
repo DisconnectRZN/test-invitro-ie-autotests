684ce5542d588a39cc908ab5b272f7d02d2fd5e0
slevanda <Disconnect62@gmail.com>
2025-07-31 21:40:15 +0300

    test

--- src/test/java/testpackage/help/Help_Methods.java
+++ src/test/java/testpackage/help/Help_Methods.java
@@ -42,73 +42,18 @@ public class Help_Methods extends PageObject {
         return INSTANCE;
     }
 
-    /*
-    Чтобы описать унифицированный метод, необходимо в него добавить переменную, как указано на примере ниже:
-
-        public void Click_Method(By test) { //Метод клика по элементу
-        find(test).waitUntilClickable().click();}
-
-    Так как язык java является строго наследуемым, необходимо точно укзывать тип переменной,
-    которую мы будем передавать в метод.
-
-    - By - переменная типа xPath
-    - String - строковое значение, содержащее текст
-    и так далее
-
-    Тип переменной указывается после наименования метода, например Click_Method, в круглых скобках (By test), где:
-
-    By - тип передаваемой переменной
-    test - имя переменной
-
-    Переменная указывается внутри необходимого метода, например find(test).
-
-    Наименование перменных можно указвыать одинаковым для нескольких методов, например:
-
-        public void Click_Method(By test) { //Метод клика по элементу
-        find(test).waitUntilClickable().click();}
-
-        public void Check_Visible_Element(By test) {
-        Assertions.assertThat(find(test).isVisible());}
-
-     Как видно из приведенных выше примеров, в каждом методе есть перменная test,
-     но так как они реализованы в разных методах, то программа/автотесты, воспринимает их как уникальные.
-    */
-
-
     public void Click_Method(By test) { //Метод клика по элементу
         find(test).waitUntilClickable().click();
         getSlow();
     }
 
-    //public void Random_RUS_Text(By test, String test1) {find(test).toString();}
-
     public void Check_Enabled_Element(By test) {
         Assertions.assertThat(find(test).isEnabled());
     }
 
-
-    /*
-     public void Click_On_All_Elements(By Xpath_Massive, By Button_Click, By Check_City, String Check_Text, By City, By Another, By Moscow) {//, By test2
-         List<WebElementFacade> massive = findAll(Xpath_Massive);
-         for (int i = 0; i <massive.size()-1; i++) {
-             massive = findAll(Xpath_Massive);
-             massive.get(i).waitUntilVisible();
-             massive.get(i).click();
-             Click_Method(Button_Click);
-
-         if (Assertions.assertThat(find(Check_City).getText()).isEqualTo(Check_Text)){
-             Change_City(City, Another, Moscow);
-             Click_Method(Button_Click);
-         }
-
-
-         }
-     }
-
-      */
     public void Verify_Text(By test, String test1) {
         Assertions.assertThat(find(test).getText()).isEqualTo(test1);
-    } //""
+    }
 
     public void Check_Visible_Element(By test) {
         Assertions.assertThat(find(test).isVisible());
--- src/test/resources/features/Certificate_invitro_web.feature
+++
@@ -1,9 +0,0 @@
-# new feature
-# Tags: optional
-    
-Feature: Проверка сертификатов
-    
-Scenario: A scenario
-    Given Открыть главную страницу сайта
-    When Открыть страницу сертификатов
-    When Проверить лицензиии по годам
