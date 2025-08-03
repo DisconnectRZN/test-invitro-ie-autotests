9741c1a177f6e4edec9b51760936c33061e47d21
Sergey Levanda <36986483+DisconnectRZN@users.noreply.github.com>
2025-08-03 11:24:30 +0300

    Update Help_Methods.java

--- src/test/java/testpackage/help/Help_Methods.java
+++ src/test/java/testpackage/help/Help_Methods.java
@@ -1,96 +1,51 @@
-package testpackage.help;
+package testpackage.pages;
 
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
-import net.thucydides.core.steps.StepEventBus;
 import org.assertj.core.api.Assertions;
-import org.openqa.selenium.By;
+import org.openqa.selenium.*;
 import org.openqa.selenium.JavascriptExecutor;
-
 import java.time.temporal.ChronoUnit;
 
-public class Help_Methods extends PageObject {
 
-    private static final Help_Methods INSTANCE = new Help_Methods();
+public class WebPage extends PageObject {
 
-    /*
-    Данный класс позволит заново не переписывать все методы, а вызывать их отсюда,
-    достаточно подставить переменную xPath, которую мы объявляем в классах Page.
-
-    Для того, чтобы было возможно обращаться к методам из этого класса в других,
-    внутри нужного нам класса необходимо прописать следующую конструкцию:
-
-    private static final Help_Methods help = Help_Methods.getInstance();
-
-    Где help - является переменной, которую можно назвать как угодно,
-    она будет распространятся только внутри класса, в котором написана
-    и для вызова метода необходимо внутри созданного метода прописать следущее:
-
-    public void Verify_Address_Button (){
-        help.Check_Visible_Element(Address_Page);}
-
-    В примере выше, мы создаем метод в классе, обращаемся к help,
-    который подтягивает методы из класса Help_Methods.class, через точку выбираем нужный метод
-    например Check_Visible_Element, для того, чтобы кликнуть по элементу.
-    В скобках вызванного метода вставить перменную содержащую xPath, например Address_Page,
-    которой соответсвует запись:
-
-    private By Address_Page = By.xpath("//a[text()='Адреса']");
-
-    Так как java достаточно умный язык, он будет показывать допустимые методы при написании тестов
-     */
-
-    private Help_Methods() {
-    }
-
-    public static Help_Methods getInstance() {
-        return INSTANCE;
-    }
-
-    public void Click_Method(By test) { //Метод клика по элементу
-        find(test).waitUntilClickable().click();
-        getSlow();
-    }
+/////////////////////////////////////////////////// Xpath's \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
     /**
-     * Вывод в консоль названия текущего шага
+     * Кнопка смены языка сайта
      */
-    public void consoleOutput() {
-        System.out.println(StepEventBus.getEventBus().getCurrentStep().toString()
-                .replaceAll("^Optional\\[", "").replaceAll("]", ""));
+    private final By enRuButton = By.xpath("//a[@class='switch-lang__icon']");
+
+
+
+/////////////////////////////////////////////////// Методы \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+    /** Метод клика по элементу */
+    public void clickMethod(By by) {
+        WebElementFacade element =
+                element(by).withTimeoutOf(30, ChronoUnit.SECONDS).waitUntilVisible();
+        scrollTo(by);
+        element.click();
     }
+
+
+    /** Метод проверки видимости элемента напрямую */
+    public void checkVisibleElement(By test, String s) {
+        WebElementFacade element = find(test).withTimeoutOf(30, ChronoUnit.SECONDS).waitUntilVisible();
+        Assertions.assertThat(element.isDisplayed()).as(test.toString() + " not visible").isTrue();
+    }
+
 
     /**
      * Скролл к нужному элементу через By
+     *
+     * @param element Элемент на странице
      */
-    public void scrollTo(By test) {
-        WebElementFacade element = find(test).withTimeoutOf(30, ChronoUnit.SECONDS).waitUntilVisible();
+    public void scrollTo(By element) {
+        WebElementFacade test = find(element).withTimeoutOf(30, ChronoUnit.SECONDS).waitUntilVisible();
         ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", test);
     }
 
-    public void Check_Enabled_Element(By test) {
-        Assertions.assertThat(find(test).isEnabled());
-    }
-
-    public void Verify_Text(By test, String test1) {
-        Assertions.assertThat(find(test).getText()).isEqualTo(test1);
-    }
-
-    public void Check_Visible_Element(By test) {
-        Assertions.assertThat(find(test).isVisible());
-    }
-
-    public void Enter_Text(By test, String test1) {
-        find(test).sendKeys(test1);
-    }
-
-    public void getSlow() { //Притормаживает выполнение на 1 секунду
-        try {
-            Thread.sleep(5000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 }
-
